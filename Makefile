9dac43ee146fcb6a6c6ad9a6ebcfba0eefec54c7
francium <varunhooda1@gmail.com>
2017-11-14 21:56:26 -0500

    Add ipc client, makefile and pipfile.

---
+++ Makefile
@@ -0,0 +1,2 @@
+serve.dev:
+	FLASK_APP=dts_sever FLASK_DEBUG=1 flask run -p 1234
--- dts_server/__main__.py
+++ dts_server/__main__.py
@@ -1,9 +1,17 @@
+# todo
+# - logging
+
 from flask import Flask, request
 
+from .ipc_client import IPC_Client
 
 app = Flask(__name__)
+ipc_client = IPC_Client('tcp://127.0.0.1:34343')
 
 
 @app.route('/data', methods=['POST'])
 def route_data():
-    return str(request.stream.read())
+    data = request.stream.read()
+    print(data)
+    with ipc_client:
+        ipc_client.transfer(data)
