ae3c7429b1e5cb58f46972cc50e2b671c3e46ccc
francium <varunhooda1@gmail.com>
2018-01-08 17:54:02 -0500

    fix(dts_server): Initializing app

--- dts_server/__init__.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':
-    from .__main__ import app
-    app.run()
--- dts_server/__main__.py
+++ dts_server/__main__.py
@@ -7,4 +7,11 @@ from .app import create_app
 
 
 # TODO args handling
+if len(argv) != 2:
+    raise Exception('Missing arguments: database')
+
 app = create_app(argv[1])
+
+if __name__ == '__main__':
+    # TODO add port argument
+    app.run()
