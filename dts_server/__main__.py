0b1e33076ea61d7ce92e3b7d9140c63a5fbc825a
francium <varunhooda1@gmail.com>
2018-02-19 19:46:07 -0500

    feat: Use env vars for port and database

--- dts_server/__main__.py
+++ dts_server/__main__.py
@@ -1,17 +1,13 @@
 # TODO
 # - logging
 
-from sys import argv
+from os import env
 
 from .app import create_app
 
 
-# TODO args handling
-if len(argv) != 2:
-    raise Exception('Missing arguments: database')
+app = create_app(env['WEEWX_DATABASE'])
 
-app = create_app(argv[1])
 
 if __name__ == '__main__':
-    # TODO add port argument
-    app.run()
+    app.run(env['DTS_PORT'])
