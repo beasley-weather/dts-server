472f03f9bcf56619ec906ffaff9cd90b06433f8b
francium <varunhooda1@gmail.com>
2018-03-04 02:28:24 -0500

    feat: Error handling

--- dts_server/app.py
+++ dts_server/app.py
@@ -1,4 +1,6 @@
 import json
+from json import JSONDecodeError
+from traceback import print_exc
 
 import weewx_orm
 from flask import Flask, request
@@ -7,15 +9,22 @@ from flask import Flask, request
 def create_app(database, into_='''
     :param database: Name of database to save into
     '''):
-    into_
     app = Flask(__name__)
 
     weewx_db = weewx_orm.WeewxDB(database)
 
     @app.route('/data', methods=['POST'])
     def route_data():
-        data = json.loads(request.stream.read())
-        weewx_db.archive_insert_data(data)
+        try:
+            data = json.loads(request.stream.read())
+            weewx_db.archive_insert_data(data)
+        except JSONDecodeError:
+            print_exc()
+            return 422, 'Invalid JSON'
+
+        except IOError:
+            print_exc()
+            return 500, 'Unable to save data'
         return ''
 
     @app.route('/')
